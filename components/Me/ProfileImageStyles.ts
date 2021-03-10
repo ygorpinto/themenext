a72bf6cc78633506081fdb54d7008336d726e927
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-03-10 12:38:54 -0300

    ok

--- components/Me/ProfileImageStyles.ts
+++ components/Me/ProfileImageStyles.ts
@@ -3,8 +3,13 @@ import styled from 'styled-components'
 export const ProfileImageStyles = styled.div`
 img {
     margin:5% 0;
-    animation: showimg 10s normal;
+    animation: showimg 6s normal;
     transition: width 2s;
+    opacity:80%;
+
+    :hover{
+        opacity:100%;
+    }
 }
 
 @keyframes showimg {
@@ -13,7 +18,7 @@ img {
     }
 
     to {
-        opacity:100%;
+        opacity:80%;
     }
 }
 `
